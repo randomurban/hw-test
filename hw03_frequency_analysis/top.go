d6bc2aac77591083fa9d9dde64bd3205926bc718
Зятнин Константин Сергеевич <kszyatn1@mts.ru>
2024-06-14 18:23:51 +0300

    add hw03 init

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(input string) []string {
+
+	tokens := strings.Fields(input)
+	counts := map[string]int{}
+
+	for _, t := range tokens {
+		counts[t]++
+
+	}
+
+	var keys []string
+	for k := range counts {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return counts[keys[i]] > counts[keys[j]]
+	})
+
+	var res []string
+	for i, k := range keys {
+		if i > 9 {
+			break
+		}
+		res = append(res, k)
+	}
+	return res
 }
