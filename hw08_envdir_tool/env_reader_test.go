c5818a0f3583bb7caecdedec91cb7127c93a5a67
Random Urban <tuqituq@yandex.ru>
2024-08-18 11:20:28 +0300

    add hw08 test newEnv

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+}
+
+func Test_newEnv(t *testing.T) {
+	type args struct {
+		osEnv []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Environment
+	}{
+		{
+			"Empty osEnv",
+			args{[]string{}},
+			Environment{},
+		},
+		{
+			"Simple osEnv",
+			args{[]string{"TESTENV=test_env"}},
+			Environment{"TESTENV": {"test_env", false}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newEnv(tt.args.osEnv); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newEnv() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
