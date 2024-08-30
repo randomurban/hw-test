a048f21c07aade9b1d3970c3adb1cfa61fa71d54
Random Urban <tuqituq@yandex.ru>
2024-08-30 22:47:23 +0300

    fix hw10 optimize GetDomainStat()

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -22,40 +22,19 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
+	result := make(DomainStat)
 	scanner := bufio.NewScanner(r)
-	i := 0
 	for scanner.Scan() {
 		var user User
-		if err = json.Unmarshal(scanner.Bytes(), &user); err != nil {
-			return result, fmt.Errorf("unmarshal error: %w", err)
+		if err := json.Unmarshal(scanner.Bytes(), &user); err != nil {
+			return nil, fmt.Errorf("unmarshal error: %w", err)
 		}
-		result[i] = user
-		if i < len(result) {
-			i++
-		} else {
-			break
-		}
-	}
-	return result, scanner.Err()
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
 		if strings.HasSuffix(user.Email, "."+domain) {
-			name := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
-			result[name]++
+			parts := strings.SplitN(user.Email, "@", 2)
+			if len(parts) == 2 {
+				name := strings.ToLower(parts[1])
+				result[name]++
+			}
 		}
 	}
 	return result, nil
