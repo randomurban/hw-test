ba9df728ecb0ce40d07ff66b19f156e38104c95a
Random Urban <tuqituq@yandex.ru>
2024-09-16 23:17:18 +0300

    add hw11 pflag

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think if it is useful with blocking operation?
+	var host string
+	var port int
+	var timeout time.Duration
+	pflag.DurationVar(&timeout, "timeout", 5*time.Second, "Timeout")
+	pflag.Parse()
+	var err error
+	host = pflag.Arg(0)
+	if host == "" {
+		usageAndExit("Empty host")
+	}
+	port, err = strconv.Atoi(pflag.Arg(1))
+	if err != nil {
+		usageAndExit("Bad port")
+	}
+	fmt.Println("host:", host)
+	fmt.Println("port:", port)
+}
+
+func usageAndExit(msg string) {
+	fmt.Printf("Usage: %s --timeout duration host port\n", os.Args[0])
+	pflag.PrintDefaults()
+	if msg != "" {
+		fmt.Println(msg)
+	}
+	os.Exit(1)
 }
