aafd61f2a54a8ced2ed6a1a49769baeaac54ba54
Random Urban <tuqituq@yandex.ru>
2024-09-27 00:32:28 +0300

    add hw12 logger

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,20 +1,60 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Logger struct { // TODO
+// error / warn / info / debug
+const (
+	DEBUG = "DEBUG"
+	INFO  = "INFO"
+	WARN  = "WARN"
+	ERROR = "ERROR"
+)
+
+type Logger struct {
+	level string
+	lev   int
 }
 
 func New(level string) *Logger {
-	return &Logger{}
+	var lev int
+	switch level {
+	case DEBUG:
+		lev = 1
+	case INFO:
+		lev = 2
+	case WARN:
+		lev = 3
+	case ERROR:
+		lev = 4
+	default:
+		lev = 0 // print Any
+	}
+	return &Logger{
+		level: level,
+		lev:   lev,
+	}
+}
+
+func (l *Logger) print(num int, msg string) {
+	if l.lev <= num {
+		fmt.Printf("%s: %s", l.level, msg)
+	}
+}
+
+func (l Logger) Debug(msg string) {
+	l.print(1, msg)
 }
 
 func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+	l.print(2, msg)
+}
+
+func (l Logger) Warn(msg string) {
+	l.print(3, msg)
 }
 
 func (l Logger) Error(msg string) {
-	// TODO
+	l.print(4, msg)
 }
-
-// TODO
