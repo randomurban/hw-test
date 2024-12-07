f728bd8a6dbd802f8c2224ba2f324d30c16d563e
Random Urban <tuqituq@yandex.ru>
2024-12-07 23:10:18 +0300

    add hw13 grpc-gw

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -23,6 +23,7 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.24.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -41,6 +42,7 @@ require (
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20241118233622-e639e219e697 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241118233622-e639e219e697 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- hw12_13_14_15_calendar/internal/server/http_server/server.go
+++ hw12_13_14_15_calendar/internal/server/http_server/server.go
@@ -6,15 +6,20 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/randomurban/hw-test/hw12_13_14_15_calendar/internal/config"
 	"github.com/randomurban/hw-test/hw12_13_14_15_calendar/internal/logger"
+	"github.com/randomurban/hw-test/hw12_13_14_15_calendar/internal/pb"
 	"github.com/randomurban/hw-test/hw12_13_14_15_calendar/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Server struct {
 	httpServer *http.Server
 	logger     logger.Logger
 	service    service.Event
+	cfg        config.Config
 }
 
 func NewServer(cfg config.Config, logger logger.Logger, service service.Event) *Server {
@@ -25,6 +30,7 @@ func NewServer(cfg config.Config, logger logger.Logger, service service.Event) *
 			ReadHeaderTimeout: cfg.HTTP.ReadHeaderTimeout,
 		},
 		service: service,
+		cfg:     cfg,
 	}
 }
 
@@ -40,10 +46,21 @@ func (s *Server) Start(ctx context.Context) error {
 		}
 	}()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", s.helloHandler)
+	mux := runtime.NewServeMux()
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/", s.helloHandler)
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	err := pb.RegisterEventServiceHandlerFromEndpoint(ctx, mux, s.cfg.GRPC.Addr, opts)
+	if err != nil {
+		return err
+	}
+
 	s.httpServer.Handler = s.loggingMiddleware(mux)
 	s.logger.Info("HTTP server started ", "addr", s.httpServer.Addr)
+
 	return s.httpServer.ListenAndServe()
 }
 
